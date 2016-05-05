11c3b6b4dc41c9e25e596f63568c712b7f797372
kevin-ledinh <kevin-ledinh@users.noreply.github.com>
2016-05-05 21:20:31 +0100

    committing skeleton android proj

---
+++ sw/EPDReader/MyApplication2/app/src/main/java/banana_tree/epdreader/MainActivity.java
@@ -0,0 +1,13 @@
+package banana_tree.epdreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
